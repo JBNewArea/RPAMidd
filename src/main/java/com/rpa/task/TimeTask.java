ec7c9987e8ae871ce342c56a13dea3c1f5906fe1
guanzheyuan <834155417@qq.com>
2018-08-05 11:18:38 +0800

    增加定时任务操作数据库案例

--- src/main/java/com/rpa/task/TimeTask.java
+++ src/main/java/com/rpa/task/TimeTask.java
@@ -2,11 +2,20 @@ package com.rpa.task;
 
 import java.util.Date;
 
+import javax.annotation.Resource;
+
+import com.rpa.pojo.User;
+import com.rpa.service.IUserService;
+
 public class TimeTask {
 
+	@Resource
+	public IUserService userService;
 	    public void execute()
 	    {
 	        System.out.println("job2 开始执行"+new Date());
+	        User user = this.userService.getUserById("");
+			System.out.println(user.getAge());
 	    }
 
 }
